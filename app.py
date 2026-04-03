c972d0bd3add2ecb8023f4b0fade684968b718bb
Dr Gary Twinn <gary@twinn.net>
2026-04-03 15:32:15 +0100

    Refactor HTML templates, implement email functionality via Microsoft Graph API, and introduce improved secrets handling

--- app.py
+++ app.py
@@ -19,21 +19,32 @@ laser hardware through the laserclass module.
 
 import subprocess
 from threading import enumerate as enumerate_threads
-from flask import Flask, render_template, jsonify, request
+from datetime import datetime
+import uuid
+from flask import Flask, render_template, jsonify, request, send_from_directory, send_file
 from laserclass import laser
-from app_control import settings, VERSION
+from app_control import settings, VERSION, SECRETS
 from logmanager import logger
+from contact_message import send_email_via_graph
 
 app = Flask(__name__)
+app.secret_key = SECRETS['api-key']
 logger.info('Starting Laser Controller web app version %s', VERSION)
-logger.info('Api-Key = %s', settings['api-key'])
+logger.info('Api-Key = %s', SECRETS['api-key'])
+YEAR = datetime.now().year
+
+def read_cpu_temperature():
+    """Read the CPU temperature and returns in Celsius"""
+    with open(settings['cputemp'], 'r', encoding='utf-8') as f:
+        log = f.readline()
+    return round(float(log) / 1000, 1)
 
 
-def read_cpu_temp(file_path):
-    """Read CPU Temp"""
+def read_log_from_file(file_path):
+    """Read a log from a file and reverse the order of the lines so newest is at the top"""
     with open(file_path, 'r', encoding='utf-8') as f:
-        line = f.readline()
-    return round(float(line) / 1000, 1)
+        lines = f.readlines()
+    return list(reversed(lines))
 
 
 def read_reverse_file(file_path):
@@ -52,13 +63,30 @@ def threadlister():
     return appthreads
 
 
+@app.errorhandler(500)
+def internal_server_error(_):
+    """
+    Handles the 500 Internal Server Error by serving a custom static HTML error page.
+
+    This function is triggered when a 500 Internal Server Error occurs in the
+    application and serves a pre-defined static HTML file designed for error handling.
+    """
+    response = send_from_directory('static', '500.html')
+    response.status_code = 500
+    return response
+
 @app.route('/')
 def index():
     """Web status page """
-    cputemperature = read_cpu_temp(settings['cputemp'])
-    return render_template('index.html', laserstatus=laser.httpstatus(),
-                           cputemperature=cputemperature, version=VERSION, threads=threadlister())
+    return render_template('index.html', settings=settings, laserstatus=laser.httpstatus(),
+                           year=YEAR, version=VERSION, threads=threadlister())
 
+@app.route('/statusdata', methods=['GET'])
+def statusdata():
+    """Status data read by javascript on default website so the page shows near live values"""
+    ctrldata = {'cputemperature': read_cpu_temperature()
+                }
+    return jsonify(ctrldata), 201
 
 @app.route('/api', methods=['POST'])
 def api():
@@ -67,58 +95,93 @@ def api():
         logger.debug('API headers: %s', request.headers)
         logger.debug('API request: %s', request.json)
         if 'Api-Key' in request.headers.keys():  # check api key exists
-            if request.headers['Api-Key'] == settings['api-key']:  # check for correct API key
+            if request.headers['Api-Key'] == SECRETS['api-key']:  # check for correct API key
                 item = request.json['item']
                 command = request.json['command']
                 status = laser.parsecontrol(item, command)
                 return jsonify(status), 201
-            logger.warning('API: access attempt using an invalid token from %s', request.headers[''])
-            return 'access token(s) unuthorised', 401
-        logger.warning('API: access attempt without a token from  %s', request.headers['X-Forwarded-For'])
-        return 'access token(s) incorrect', 401
+            logger.warning('API: access attempt using an invalid token')
+            return {'status': 'access token(s) unauthorised'}, 401
+        logger.warning('API: access attempt without a token')
+        return {'status': 'access token(s) incorrect'}, 401
     except KeyError:
-        logger.warning('API endpoint: bad json message')
-        return "badly formed json message", 201
+        logger.warning('API: KeyError with json message')
+        return {'status': "badly formed json message"}, 400
 
 
 @app.route('/pylog')
 def showplogs():
-    """Show apoplication log"""
-    cputemperature = read_cpu_temp(settings['cputemp'])
-    logs = read_reverse_file(settings['logfilepath'])
-    return render_template('logs.html', rows=logs, log='Laser Controller Log',
-                           cputemperature=cputemperature, version=VERSION)
+    """Show the Application log web page"""
+    cputemperature = read_cpu_temperature()
+    logs = read_log_from_file(settings['logfilepath'])
+    return render_template('logviewer.html', rows=logs, log='Application log',
+                           cputemperature=cputemperature, settings=settings, version=VERSION, year=YEAR)
 
 
 @app.route('/guaccesslog')
 def showgalogs():
-    """Gunicorn access log page"""
-    cputemperature = read_cpu_temp(settings['cputemp'])
-    logs = read_reverse_file(settings['gunicornpath'] + 'gunicorn-access.log')
-    return render_template('logs.html', rows=logs, log='Gunicorn Access Log',
-                           cputemperature=cputemperature, version=VERSION)
+    """"Show the Gunicorn Access Log web page"""
+    cputemperature = read_cpu_temperature()
+    logs = read_log_from_file(settings['gunicornpath'] + 'gunicorn-access.log')
+    return render_template('logviewer.html', rows=logs, log='Gunicorn Access Log',
+                           cputemperature=cputemperature, settings=settings, version=VERSION, year=YEAR)
 
 
 @app.route('/guerrorlog')
 def showgelogs():
-    """Gunicorn error log page"""
-    cputemperature = read_cpu_temp(settings['cputemp'])
-    logs = read_reverse_file(settings['gunicornpath'] + 'gunicorn-error.log')
-    return render_template('logs.html', rows=logs, log='Gunicorn Error Log',
-                           cputemperature=cputemperature, version=VERSION)
+    """"Show the Gunicorn Errors Log web page"""
+    cputemperature = read_cpu_temperature()
+    logs = read_log_from_file(settings['gunicornpath'] + 'gunicorn-error.log')
+    return render_template('logviewer.html', rows=logs, log='Gunicorn Error Log',
+                           cputemperature=cputemperature, settings=settings, version=VERSION, year=YEAR)
 
 
-@app.route('/syslog')  # display the raspberry pi system log
+@app.route('/syslog')
 def showslogs():
-    """System log page - shows 200 most recent messages"""
-    cputemperature = read_cpu_temp(settings['cputemp'])
-    log = subprocess.Popen('journalctl -n 200', shell=True,
+    """Show the last 2000 lines from the system log on a web page"""
+    cputemperature = read_cpu_temperature()
+    log = subprocess.Popen('/bin/journalctl -n 2000', shell=True,
                            stdout=subprocess.PIPE).stdout.read().decode(encoding='utf-8')
     logs = log.split('\n')
     logs.reverse()
-    return render_template('logs.html', rows=logs, log='System Log',
-                           cputemperature=cputemperature, version=VERSION)
+    return render_template('logviewer.html', rows=logs, log='System Log', cputemperature=cputemperature,
+                            settings=settings, version=VERSION, year=YEAR)
 
+@app.route('/documentation')
+def download_manual():
+    """
+    Handles the request to download the application's manual.
+
+    This function serves the PDF manual of the application as a downloadable
+    attachment. The manual file's name is retrieved from the application
+    settings and provided as the download name.
+    """
+    return send_file('manual.pdf', download_name='%s.pdf' % settings['app-name'], as_attachment=True)
+
+
+@app.route('/pp')
+def privacy_policy():
+    """Show the privacy policy page"""
+    return render_template('pp.html', year=YEAR, version=VERSION, settings=settings)
+
+
+@app.route('/contactform')
+def contact_us():
+    """Handles the HTTP request for the contact form page."""
+    return render_template('contact.html', formname='contact', year=YEAR, version=VERSION, settings=settings)
+
+
+@app.route('/support')
+def support_ticket():
+    """Handles the HTTP request for the support form page."""
+    return render_template('contact.html', ticket=uuid.uuid4().hex, formname='support', year=YEAR, version=VERSION, settings=settings)
+
+
+@app.route('/contactresponse', methods=['POST'])
+def contact_reply():
+    """Reply page on sucecssful submission of contact form."""
+    send_email_via_graph(request.form)
+    return render_template('contactresponse.html', request_data=request.form, year=YEAR, version=VERSION, settings=settings)
 
 if __name__ == '__main__':
     app.run()
--- app_control.py
+++ app_control.py
@@ -4,26 +4,29 @@ has appeared it will creat from the defaults in the initialise function.
 """
 import random
 import json
+from shutil import copyfile
+from base64 import b64decode, b64encode
 from datetime import datetime
 
-VERSION = '2.1.0'
+VERSION = '2.2.0'
 
 
 def initialise():
     """These are the default values written to the settings.json file the first time the app is run"""
-    isettings = {'LastSave': '01/01/2000 00:00:01',
+    default_settings = {'LastSave': '01/01/2000 00:00:01',
                  'app-name': 'UCL Helium Line Laser Controller',
-                 'api-key': 'change-me',
+                 'api-key-date': '01/01/2000 00:00:01',
                  'logfilepath': './logs/lasercontrol.log',
                  'logappname': 'LaserControler-Py',
                  'loglevel': 'INFO',
+                 'email_recipients': 'info@tstechnologies.co.uk',
                  'gunicornpath': './logs/',
                  'cputemp': '/sys/class/thermal/thermal_zone0/temp',
                  'port': '/dev/ttyS0',
                  'baud': 9600,
                  'power': 30,
                  'maxtime': 300}
-    return isettings
+    return default_settings
 
 
 def generate_api_key(key_len):
@@ -43,29 +46,60 @@ def readsettings():
     """Read the settings from the json file"""
     try:
         with open('settings.json', 'r', encoding='utf-8') as json_file:
-            jsettings = json.load(json_file)
-            return jsettings
+            return json.load(json_file)
     except FileNotFoundError:
         print('File not found')
         return {}
 
-def loadsettings():
-    """Takes the settings vairable from the initiqalise and replaces valuesa from the json file if they exist"""
+
+def load_settings():
+    """Takes the settings variable from the initialise and replaces values from the json file if they exist"""
     global settings
-    settingschanged = False
-    fsettings = readsettings()
+    settings_changed = False
+    default_settings = readsettings()
     for item in settings.keys():
         try:
-            settings[item] = fsettings[item]
+            settings[item] = default_settings[item]
         except KeyError:
             print('settings[%s] Not found in json file using default' % item)
-            settingschanged = True
-    if settings['api-key'] == 'change-me':
-        settings['api-key'] = generate_api_key(30)
-        settingschanged = True
-    if settingschanged:
+            settings_changed = True
+    if settings['api-key-date'] == '01/01/2000 00:00:01':  # Needs setting
+        update_secret('api-key', generate_api_key(128))
+        settings['api-key-date'] = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        settings_changed = True
+    if settings_changed:
         writesettings()
 
 
+def load_secrets():
+    """
+    Load secrets from a file and decode them.
+
+    This function reads a file named 'SECRETS', decodes its contents using Base64,
+    and then parses the resulting JSON. It is used to securely retrieve stored
+    configuration or sensitive data. The file is expected to contain secrets
+    encoded in a specific format.
+    """
+    with open('SECRETS', 'r', encoding='utf-8') as s_file:
+        raw_secrets = s_file.read()
+    s_file.close()
+    return json.loads(b64decode(raw_secrets))
+
+
+def update_secret(key, value):
+    """
+    Updates the secret storage by adding or updating a key-value pair. The method also creates a
+    backup of the existing storage file before writing the updated encoded secrets back to the file.
+    """
+    global SECRETS
+    SECRETS[key] = value
+    new_secret = b64encode(json.dumps(SECRETS).encode('utf-8')).decode('utf-8')
+    copyfile('SECRETS', 'SECRETS.bak')
+    with open('SECRETS', 'w', encoding='utf-8') as s_file:
+        s_file.write(new_secret)
+    s_file.close()
+
+
+SECRETS = load_secrets()
 settings = initialise()
-loadsettings()
+load_settings()
